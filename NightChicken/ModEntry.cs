5c2cfb37bccf53f8b47bf10972119521bb85c38a
YonKuma <kumarei@gmail.com>
2017-10-30 20:06:40 -0400

    Initial setup

---
+++ NightChicken/ModEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StardewModdingAPI;
+using System.Threading.Tasks;
+
+namespace NightChicken
+{
+    class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            // TODO: Implement
+        }
+    }
+}
